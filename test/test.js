b6debd6d3596b51f0acd6e1c146f5dc926fb5ff2
Alejandro Arzola García <alu0100814712@ull.edu.es>
2017-04-25 12:30:12 +0100

    Añadiendo pruebas y gulpfile

---
+++ test/test.js
@@ -0,0 +1,31 @@
+let Shape = require('@alejandro-carlos-samuel/ull-shape');
+let Triangle = require('../lib/triangle.js');
+let assert = require('chai').assert;
+let expect = require('chai').expect;
+
+describe("Shape", function() {
+  it("Se crea correctamente el objeto Shape", function() {
+    let a = new Shape({ width: 100, height: 50 }, 'Triangle');
+    a.should.not.undefined();
+  })
+});
+
+describe("Triangle", function() {
+  it("Se crea correctamente el objeto Triangle", function() {
+    let a = new Triangle({ width: 100, height: 50 });
+    a.should.not.undefined();
+  })
+});
+
+describe("getArea", function() {
+  it("Se calcula correctamente el área usando Shape", function() {
+    let a = new Shape({ width: 100, height: 50 }, 'Triangle');
+    let s = a.getArea();
+    s.should.match(2500);
+  })
+  it("Se calcula correctamente el área usando Triangle", function() {
+    let a = new Triangle({ width: 200, height: 200 });
+    let s = a.getArea();
+    s.should.match(20000);
+  })
+});
